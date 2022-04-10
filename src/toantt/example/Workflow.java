ecaa0af23dad936505f8e8d1fc20f8e37e22e49c
toantt@bap.jp <toantt@bap.jp>
2022-04-10 14:46:56 +0700

    first commit

---
+++ src/toantt/example/Workflow.java
@@ -0,0 +1,78 @@
+package toantt.example;
+
+import java.util.Date;
+
+public class Workflow {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	private String name;
+	private boolean status;
+	private Date start;
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	private Date end;
+	private Workflow link;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Workflow getLink() {
+		return link;
+	}
+
+	public void setLink(Workflow link) {
+		this.link = link;
+	}
+
+	/*
+	 * This is a function to find a workflow by id
+	 */
+	private Workflow findWorkById(Long id) {
+		
+		return null;
+	}
+	
+	/*
+	 * This is a function to find a workflow by name
+	 */
+	private Workflow findWorkByName(String name) {
+		
+		return null;
+	}
+}
